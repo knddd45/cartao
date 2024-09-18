2f7dda5f8e6b9fbcd24b6dd93407463a6525cef7
knddd45 <160049610+knddd45@users.noreply.github.com>
2024-09-18 14:42:34 -0300

    knkz

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'qual o  nome do carro bomba brasileiro',
+    'marea',
+
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'oque o peixe falou pro outro',
+    'nada',
+    
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'matematica',
+    'quanto e 5-1',
+    'o resultado e 4'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
-)
+    'matematica',
+'quanto e 1-1?',
+    '0 o resultado'
+)   
