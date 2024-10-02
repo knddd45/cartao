0cb9f504d7c6ed158bf18c5467e3cd3a4d00e259
knddd45 <160049610+knddd45@users.noreply.github.com>
2024-10-02 14:35:21 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,10 @@
 criaCartao(
-    'qual o  nome do carro bomba brasileiro',
-    'marea',
-
-)
+    'pergunta',
+    'qual o nome do carro bomba br',
+     'marea',
 
 criaCartao(
+     'pergunta',
     'oque o peixe falou pro outro',
     'nada',
     
@@ -18,6 +18,40 @@ criaCartao(
 
 criaCartao(
     'matematica',
-'quanto e 1-1?',
-    '0 o resultado'
+'quanto e 3-1?',
+    '0 -2 resultado'
+    
+)   criaCartao(
+    'pergunta',
+    'qual o nome do carro bomba br',
+     'marea',
+
+criaCartao(
+     'pergunta',
+    'oque o peixe falou pro outro',
+    'vai,nada',
+    
+)
+
+criaCartao(
+    'matematica',
+    'quanto e 4-1',
+    'o resultado e 3'
+)
+
+criaCartao(
+    'matematica',
+'quanto e 2-1?',
+    '0 1 resultado'
+)   
+criaCartao(
+    'matematica',
+    'quanto e 5-1',
+    'o resultado e 4'
+)
+
+criaCartao(
+    'matematica',
+'quanto e 3-1?',
+    '0 2 resultado'
 )   
